6c1fe1ef3324f726d0f4147abecddc4d1fe0bf01
Samuel <eb.samuel6@gmail.com>
2023-11-08 01:31:18 -0500

    Tried to connect with Firebase

--- src/App.js
+++ src/App.js
@@ -1,103 +1,57 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import { collection, getDocs, addDoc, doc, updateDoc } from 'firebase/firestore';
+// import { db } from './firebase/firebaseConfig';
 import Filters from './layouts/filters';
 import RoadMap from './layouts/roadmap';
-import Search from "./layouts/search";
+import Search from './layouts/search';
 
 function App() {
 
-    const [filters, setFilters] = useState([]);
-    const [features, setFeatures] = useState([
-        {
-            id: 1,
-            title: 'Revenue Tracking',
-            description: 'Add ability to track revenue lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            tag: 'Bug Reports',
-            status: 'Open',
-            votes: 16
-        },
-        {
-            id: 2,
-            title: 'Revenue Tracking 2',
-            description: 'Add ability to track revenue1 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            tag: 'Bug Reports',
-            status: 'In Progress',
-            votes: 2
-        },
-        {
-            id:3,
-            title: 'Revenue Tracking 3',
-            description: 'Add ability to track revenue2',
-            tag: 'Bug Reports',
-            status: 'Done',
-            votes: 4
-        },
-        {
-            id: 4,
-            title: 'Revenue Tracking 4',
-            description: 'Add ability to track revenue3',
-            tag: 'Integrations',
-            status: 'Open',
-            votes: 8
-        },
-        {
-            id: 5,
-            title: 'Revenue Tracking 5',
-            description: 'Add ability to track revenue4',
-            tag: 'Integrations',
-            status: 'In Progress',
-            votes: 10
-        },
-        {
-            id: 6,
-            title: 'Revenue Tracking 6',
-            description: 'Add ability to track revenue5',
-            tag: 'Integrations',
-            status: 'Done',
-            votes: 12
-        },
-        {
-            id: 7,
-            title: 'Revenue Tracking 7',
-            description: 'Add ability to track revenue6',
-            tag: 'Feature Requests',
-            status: 'Open',
-            votes: 14
-        },
-        {
-            id: 8,
-            title: 'Revenue Tracking 8',
-            description: 'Add ability to track revenue7',
-            tag: 'Feature Requests',
-            status: 'In Progress',
-            votes: 16
-        },
-        {
-            id: 9,
-            title: 'Revenue Tracking 9',
-            description: 'Add ability to track revenue8',
-            tag: 'Feature Requests',
-            status: 'Done',
-            votes: 18
-        }
-    ]);
-
-    useEffect(() => {
-        const mFilters = {};
-        features.forEach(feature => {
-            if (!mFilters[feature.tag]) mFilters[feature.tag] = 1;
-            else mFilters[feature.tag]++;
-        });
-        const newFilters = [];
-        for (const filter in mFilters) {
-            newFilters.push({ name: filter, amount: mFilters[filter] });
-        }
-        setFilters(newFilters);
-    }, [features]);
-
     const [selectedFilter, setSelectedFilter] = useState(null);
     const [searchText, setSearchText] = useState("");
+    const [filters, setFilters] = useState([]);
+    const [features, setFeatures] = useState([]);
 
+    const firebaseConfig = {
+        apiKey: "AIzaSyCAD9wmH7LYp2zTm4n8y0eJlfvgjSi8WMI",
+        authDomain: "roadmap-test0.firebaseapp.com",
+        projectId: "roadmap-test0",
+        storageBucket: "roadmap-test0.appspot.com",
+        messagingSenderId: "793314467036",
+        appId: "1:793314467036:web:56ad552545064fd2a7e819"
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const featuresCollection = collection(db, process.env.REACT_APP_DATABASE_NAME);
+            const featuresSnapshot = await getDocs(featuresCollection);
+            const featureData = featuresSnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setFeatures(featureData);
+        };
+
+        fetchData();
+    }, [features]);
+
+
+
+    useEffect(() => {
+
+            const mFilters = {};
+            features.forEach((feature) => {
+                if (!mFilters[feature.tag]) mFilters[feature.tag] = 1;
+                else mFilters[feature.tag]++;
+            });
+            const newFilters = [];
+            for (const filter in mFilters) {
+                newFilters.push({ name: filter, amount: mFilters[filter] });
+            }
+            setFilters(newFilters);
+
+    }, [features, ]);
 
     const handleFilterSelect = (filterName) => {
         if (selectedFilter === filterName) setSelectedFilter(null);
@@ -108,31 +62,60 @@ function App() {
         setSearchText(e.target.value);
     };
 
-    const notifyParentVotesChanged = (id, amount) => {
-        const featuresCopy = [...features];
-        const mElem = featuresCopy.find((elem) => elem.id === id);
-        mElem.votes = amount;
-        setFeatures(featuresCopy);
-    }
+    const notifyParentVotesChanged = async (id, amount) => {
+        try {
+            const featureRef = doc(db, process.env.REACT_APP_DATABASE_NAME, id);
+            await updateDoc(featureRef, { votes: amount });
 
-    const onFeatureCreate = (newFeature) => {
-        const updatedFeatures = [...features, newFeature];
-        setFeatures(updatedFeatures);
+
+            const featuresCopy = [...features];
+            const mElem = featuresCopy.find((elem) => elem.id === id);
+            mElem.votes = amount;
+            setFeatures(featuresCopy);
+        } catch (error) {
+            console.error("Error updating votes in database", error);
+        }
     };
 
-    console.log('features',features)
+    const onFeatureCreate = async (newFeature) => {
+        try {
 
-    const allStatus = [...(new Set(features.map((feature) => feature.status)))];
+            const docRef = await addDoc(collection(db, process.env.REACT_APP_DATABASE_NAME), newFeature);
+
+
+            setFeatures([...features, { id: docRef.id, ...newFeature }]);
+        } catch (error) {
+            console.error("Could not create a new feature in the database:", error);
+        }
+    };
+
+    const allStatus = [...new Set(features.map((feature) => feature.status))];
 
     const filteredFeatures = selectedFilter
-        ? features.filter((feature) => feature.tag === selectedFilter && feature.title.toLowerCase().includes(searchText.toLowerCase()))
-        : features.filter((feature) => feature.title.toLowerCase().includes(searchText.toLowerCase()));
+        ? features.filter(
+            (feature) =>
+                feature.tag === selectedFilter &&
+                feature.title.toLowerCase().includes(searchText.toLowerCase())
+        )
+        : features.filter((feature) =>
+            feature.title.toLowerCase().includes(searchText.toLowerCase())
+        );
+
 
     return (
-        <div className=' container containerApp'>
-            <Filters filters={filters} onSelectFilter={handleFilterSelect} selectedFilter={selectedFilter} />
+        <div className="container containerApp">
+            <Filters
+                filters={filters}
+                onSelectFilter={handleFilterSelect}
+                selectedFilter={selectedFilter}
+            />
             <Search searchText={searchText} onSearchChange={handleSearchInputChange} />
-            <RoadMap notifyParentVotesChanged={notifyParentVotesChanged} features={filteredFeatures} status={allStatus} onFeatureCreate={onFeatureCreate} />
+            <RoadMap
+                notifyParentVotesChanged={notifyParentVotesChanged}
+                features={filteredFeatures}
+                status={allStatus}
+                onFeatureCreate={onFeatureCreate}
+            />
         </div>
     );
 }
